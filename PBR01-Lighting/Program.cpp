cede6041d55fbf90462cb4f04eb171934f50511c
Zou Wei <zwcloud@hotmail.com>
2019-05-18 20:27:02 +0800

    arrange and clean

--- PBR01-Lighting/Program.cpp
+++ PBR01-Lighting/Program.cpp
@@ -23,7 +23,7 @@ GLuint vertexBuf = 0;
 GLuint indexBuf = 0;
 GLuint vShader = 0;
 GLuint pShader = 0;
-GLuint normalProgram = 0;
+GLuint program = 0;
 GLint attributePos = 0;
 GLint attributeTexCoord = 0;
 GLint attributeNormal = 0;
@@ -319,160 +319,13 @@ GLuint CreateShaderProgram(const char* vShaderStr, const char* pShaderStr)
 }
 
 #include "Sphere.hpp"
+#include "VertexShader.hpp"
+#include "FragmentShader.hpp"
 
 BOOL InitOpenGL(HWND hWnd)
 {
-    GLint compiled;
-    {
-        const char* vShaderStr = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoords;
-layout (location = 2) in vec3 aNormal;
-
-out vec2 TexCoords;
-out vec3 WorldPos;
-out vec3 Normal;
-
-uniform mat4 projection;
-uniform mat4 view;
-uniform mat4 model;
-
-void main()
-{
-    TexCoords = aTexCoords;
-    WorldPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(model) * aNormal;
-
-    gl_Position =  projection * view * vec4(WorldPos, 1.0);
-}
-)";
-        const char* pShaderStr = R"(
-#version 330 core
-out vec4 FragColor;
-in vec2 TexCoords;
-in vec3 WorldPos;
-in vec3 Normal;
-
-// material parameters
-uniform vec3 albedo;
-uniform float metallic;
-uniform float roughness;
-uniform float ao;
-
-// lights
-uniform vec3 lightPositions[4];
-uniform vec3 lightColors[4];
-
-uniform vec3 camPos;
-
-const float PI = 3.14159265359;
-// ----------------------------------------------------------------------------
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a = roughness*roughness;
-    float a2 = a*a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return nom / max(denom, 0.001); // prevent divide by zero for roughness=0.0 and NdotH=1.0
-}
-// ----------------------------------------------------------------------------
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-
-    float nom   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-
-    return nom / denom;
-}
-// ----------------------------------------------------------------------------
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-
-    return ggx1 * ggx2;
-}
-// ----------------------------------------------------------------------------
-vec3 fresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-// ----------------------------------------------------------------------------
-void main()
-{
-    vec3 N = normalize(Normal);
-    vec3 V = normalize(camPos - WorldPos);
-
-    // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0
-    // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)
-    vec3 F0 = vec3(0.04);
-    F0 = mix(F0, albedo, metallic);
-
-    // reflectance equation
-    vec3 Lo = vec3(0.0);
-    for(int i = 0; i < 4; ++i)
-    {
-        // calculate per-light radiance
-        vec3 L = normalize(lightPositions[i] - WorldPos);
-        vec3 H = normalize(V + L);
-        float distance = length(lightPositions[i] - WorldPos);
-        float attenuation = 1.0 / (distance * distance);
-        vec3 radiance = lightColors[i] * attenuation;
-
-        // Cook-Torrance BRDF
-        float NDF = DistributionGGX(N, H, roughness);
-        float G   = GeometrySmith(N, V, L, roughness);
-        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
-
-        vec3 nominator    = NDF * G * F;
-        float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
-        vec3 specular = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
-
-        // kS is equal to Fresnel
-        vec3 kS = F;
-        // for energy conservation, the diffuse and specular light can't
-        // be above 1.0 (unless the surface emits light); to preserve this
-        // relationship the diffuse component (kD) should equal 1.0 - kS.
-        vec3 kD = vec3(1.0) - kS;
-        // multiply kD by the inverse metalness such that only non-metals
-        // have diffuse lighting, or a linear blend if partly metal (pure metals
-        // have no diffuse light).
-        kD *= 1.0 - metallic;
-
-        // scale light by NdotL
-        float NdotL = max(dot(N, L), 0.0);
-
-        // add to outgoing radiance Lo
-        Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
-    }
-
-    // ambient lighting (note that the next IBL tutorial will replace
-    // this ambient lighting with environment lighting).
-    vec3 ambient = vec3(0.03) * albedo * ao;
-
-    vec3 color = ambient + Lo;
-
-    // HDR tonemapping
-    color = color / (color + vec3(1.0));
-    // gamma correct
-    color = pow(color, vec3(1.0/2.2));
-
-    //FragColor = vec4(color.r*0.001 + 0.5, 0.0, 0.0, 1.0);
-    FragColor = vec4(color, 1.0);
-}
-)";
-        normalProgram = CreateShaderProgram(vShaderStr, pShaderStr);
-    }
+    //create program
+    program = CreateShaderProgram(vShaderStr, pShaderStr);
     _CheckGLError_
 
     //attribute
@@ -480,25 +333,25 @@ void main()
     attributeTexCoord = 1;
     attributeNormal = 2;
     //mvp
-    uniformModel = glGetUniformLocation(normalProgram, "model");
-    uniformView = glGetUniformLocation(normalProgram, "view");
-    uniformProjection = glGetUniformLocation(normalProgram, "projection");
+    uniformModel = glGetUniformLocation(program, "model");
+    uniformView = glGetUniformLocation(program, "view");
+    uniformProjection = glGetUniformLocation(program, "projection");
 
     // material parameters
-    GLint uniformAlbedo = glGetUniformLocation(normalProgram, "albedo");
-    GLint uniformMetallic = glGetUniformLocation(normalProgram, "metallic");
-    GLint uniformRoughness = glGetUniformLocation(normalProgram, "roughness");
-    GLint uniformAo = glGetUniformLocation(normalProgram, "ao");
+    GLint uniformAlbedo = glGetUniformLocation(program, "albedo");
+    GLint uniformMetallic = glGetUniformLocation(program, "metallic");
+    GLint uniformRoughness = glGetUniformLocation(program, "roughness");
+    GLint uniformAo = glGetUniformLocation(program, "ao");
 
     // lights
-    GLint uniformLightPositions = glGetUniformLocation(normalProgram, "lightPositions");
-    GLint uniformLightColors = glGetUniformLocation(normalProgram, "lightColors");
+    GLint uniformLightPositions = glGetUniformLocation(program, "lightPositions");
+    GLint uniformLightColors = glGetUniformLocation(program, "lightColors");
 
-    GLint uniformCamPos = glGetUniformLocation(normalProgram, "camPos");
+    GLint uniformCamPos = glGetUniformLocation(program, "camPos");
 
     _CheckGLError_
 
-    glUseProgram(normalProgram);
+    glUseProgram(program);
     //MVP
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)clientWidth / (float)clientHeight, 0.1f, 100.0f);
     glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
@@ -576,7 +429,7 @@ void DestroyOpenGL(HWND hWnd)
     //OpenGL Destroy
     glDeleteShader(vShader);
     glDeleteShader(pShader);
-    glDeleteProgram(normalProgram);
+    glDeleteProgram(program);
 
     //OpenGL Context Destroy
     wglMakeCurrent(NULL, NULL);
@@ -592,7 +445,7 @@ void Render(HWND hWnd)
     // draw
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuf);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuf);
-    glUseProgram(normalProgram);
+    glUseProgram(program);
     glDrawElements(GL_TRIANGLES, sizeof(indices)/3, GL_UNSIGNED_INT, 0);
 
     _CheckGLError_
