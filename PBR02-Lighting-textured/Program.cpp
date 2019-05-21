5be6e00dc40fd88febbcac9c74ad4bffea9e5c13
Zou Wei <zwcloud@hotmail.com>
2019-05-21 21:20:52 +0800

    fixed sampler settings

--- PBR02-Lighting-textured/Program.cpp
+++ PBR02-Lighting-textured/Program.cpp
@@ -355,10 +355,10 @@ GLuint CreateTexture(const char* imagePath)
     free(image);
 
     //sampler settings
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     return texture;
 }
